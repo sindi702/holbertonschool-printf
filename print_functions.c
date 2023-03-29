81b4b1bce302bee6ec28a9ec9376862ffa71712c
sindi702 <hamatajsindi@gmail.com>
2023-03-29 13:04:48 -0700

    printf

--- print_functions.c
+++ print_functions.c
@@ -9,6 +9,7 @@
 int p_char(va_list list)
 {
 	char c = va_arg(list, int);
+
 	_putchar(c);
 	return (1);
 }
@@ -23,7 +24,7 @@ int p_str(va_list list)
 	char *str;
 
 	str = va_arg(list, char*);
-	if (str ==NULL)
+	if (str == NULL)
 		str = "(null)";
 	for (i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
